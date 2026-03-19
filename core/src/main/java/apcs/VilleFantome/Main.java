440ec83d1e9942254d6611784356152c359e9299
albs31 <alfibs31@gmail.com>
2026-03-19 09:42:33 -0700

    idk bro im tyring stuff legit changed nothing

--- core/src/main/java/apcs/VilleFantome/Main.java
+++ core/src/main/java/apcs/VilleFantome/Main.java
@@ -4,26 +4,25 @@ import com.badlogic.gdx.Game;
 
 public class Main extends Game {
 
-    private MusicPlayer player; // Defining it here so we can use it later
+    private MusicPlayer player; 
 
     @Override
     public void create() {
-        // 1. Initialize and play the music
+        // LibGDX is now ready! We can load files here.
         player = new MusicPlayer();
         player.playMusic();
 
-        // 2. Start the loading screen
+        // Start your loading screen
         setScreen(new LoadingScreen()); 
     }
 
     @Override
     public void render() {
-        super.render(); // This is required for screens to show up!
+        super.render(); // Essential for the screen to show up
     }
 
     @Override
     public void dispose() {
-        // Clean up everything when the game closes
         if (getScreen() != null) getScreen().dispose();
         if (player != null) player.stopMusic();
     }

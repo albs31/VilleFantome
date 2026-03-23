3cbfbb16d778b0865a34715fb1432dd70993c3d2
suhas <suhassr@icloud.com>
2026-03-22 23:15:59 -0700

    Update LoadingScreen.java, completely revamped + new button methods

--- core/src/main/java/apcs/VilleFantome/LoadingScreen.java
+++ core/src/main/java/apcs/VilleFantome/LoadingScreen.java
@@ -3,24 +3,23 @@ package apcs.VilleFantome;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Rectangle;
-import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
-import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class LoadingScreen implements Screen {
     private Main game;
     private SpriteBatch batch;
     private Texture background;
-    private Texture debugTex; // For the highlight
-    private OrthographicCamera camera;
-    private Viewport viewport;
-    private Rectangle playButtonBounds;
-    private Vector3 touchPoint;
+    private Stage stage;
+
+    private Texture playTex, saveTex, loreTex;
 
     public LoadingScreen(Main game) {
         this.game = game;
@@ -30,24 +29,55 @@ public class LoadingScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         background = new Texture("finalloading.png");
-        
-        // Create a 1x1 white texture for the debug box
-        Pixmap pixmap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
-        pixmap.setColor(1, 1, 1, 1);
-        pixmap.fill();
-        debugTex = new Texture(pixmap);
-        pixmap.dispose();
 
-        camera = new OrthographicCamera();
-        viewport = new FitViewport(1280, 720, camera);
-        viewport.apply();
-        camera.position.set(640, 360, 0);
+        stage = new Stage(new FitViewport(1280, 720));
+        Gdx.input.setInputProcessor(stage);
 
-        // ADJUST THESE to move the box: (x, y, width, height)
-        // show() method in LoadingScreen.java
-// Increase X to move right, Decrease Y to move down
-playButtonBounds = new Rectangle(330, 307, 150, 60); 
-        touchPoint = new Vector3();
+        // --- Play Button ---
+        playTex = new Texture("play_button.png");
+        ImageButton playButton = new ImageButton(
+            new TextureRegionDrawable(new TextureRegion(playTex))
+        );
+        playButton.setPosition(327, 305);
+        playButton.setSize(155, 72);
+        playButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new WarningScreen(game));
+            }
+        });
+
+        // --- Save Button ---
+        saveTex = new Texture("save_button.png");
+        ImageButton saveButton = new ImageButton(
+            new TextureRegionDrawable(new TextureRegion(saveTex))
+        );
+        saveButton.setPosition(327, 220);
+        saveButton.setSize(155, 72);
+        saveButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new WarningScreen(game)); // replace with SaveScreen(game) when ready
+            }
+        });
+
+        // --- Lore Button ---
+        loreTex = new Texture("lore_button.png");
+        ImageButton loreButton = new ImageButton(
+            new TextureRegionDrawable(new TextureRegion(loreTex))
+        );
+        loreButton.setPosition(327, 126);
+        loreButton.setSize(155, 72);
+        loreButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                game.setScreen(new WarningScreen(game)); // replace with LoreScreen(game) when ready
+            }
+        });
+
+        stage.addActor(playButton);
+        stage.addActor(saveButton);
+        stage.addActor(loreButton);
     }
 
     @Override
@@ -55,36 +85,35 @@ playButtonBounds = new Rectangle(330, 307, 150, 60);
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        camera.update();
-        batch.setProjectionMatrix(camera.combined);
-
+        batch.setProjectionMatrix(stage.getCamera().combined);
         batch.begin();
         batch.draw(background, 0, 0, 1280, 720);
-
-
         batch.end();
 
-        if (Gdx.input.justTouched()) {
-            touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-            viewport.unproject(touchPoint);
-
-            if (playButtonBounds.contains(touchPoint.x, touchPoint.y)) {
-                game.setScreen(new WarningScreen(game)); 
-            }
-        }
+        stage.act(delta);
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        if (viewport != null) viewport.update(width, height, true);
+        stage.getViewport().update(width, height, true);
     }
 
-    @Override public void dispose() {
+    @Override
+    public void dispose() {
         batch.dispose();
         background.dispose();
-        debugTex.dispose();
+        stage.dispose();
+        playTex.dispose();
+        saveTex.dispose();
+        loreTex.dispose();
     }
-    @Override public void hide() {}
+
+    @Override
+    public void hide() {
+        Gdx.input.setInputProcessor(null);
+    }
+
     @Override public void pause() {}
     @Override public void resume() {}
 }

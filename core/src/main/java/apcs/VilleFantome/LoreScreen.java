29c1b40ee292353d9c52f7333865d64f6598faea
suhas <suhassr@icloud.com>
2026-03-22 23:27:03 -0700

    Create LoreScreen.java

---
+++ core/src/main/java/apcs/VilleFantome/LoreScreen.java
@@ -0,0 +1,59 @@
+package apcs.VilleFantome;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+public class LoreScreen implements Screen {
+    private Main game;
+    private SpriteBatch batch;
+    private Texture loreImage;
+    private Stage stage;
+
+    public LoreScreen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        loreImage = new Texture("lore.png");
+
+        stage = new Stage(new FitViewport(1280, 720));
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(stage.getCamera().combined);
+        batch.begin();
+        batch.draw(loreImage, 0, 0, 1280, 720);
+        batch.end();
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        loreImage.dispose();
+        stage.dispose();
+    }
+
+    @Override public void hide() { Gdx.input.setInputProcessor(null); }
+    @Override public void pause() {}
+    @Override public void resume() {}
+}

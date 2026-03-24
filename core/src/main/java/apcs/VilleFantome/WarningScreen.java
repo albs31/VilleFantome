d3fd328e9ace02b318f3ade702eddf39b09c85a4
Riti <ritikarthikeyan@gmail.com>
2026-03-23 21:56:44 -0700

    Refactor WarningScreen to remove unused variables

--- core/src/main/java/apcs/VilleFantome/WarningScreen.java
+++ core/src/main/java/apcs/VilleFantome/WarningScreen.java
@@ -1,27 +1,21 @@
 package apcs.VilleFantome;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class WarningScreen implements Screen {
     private Main game;
     private SpriteBatch batch;
     private Texture warningTexture;
-    private Texture blackTexture; 
-    private Stage stage;
-    private boolean inputSet = false;
-    private boolean showControls = false;
-
-    private float fadeAlpha = 0.0f; 
-    private float fadeSpeed = 2.5f; 
-    private boolean isExiting = false; 
+    private OrthographicCamera camera;
+    private Viewport viewport;
 
     public WarningScreen(Main game) {
         this.game = game;
@@ -31,68 +25,45 @@ public class WarningScreen implements Screen {
     public void show() {
         batch = new SpriteBatch();
         warningTexture = new Texture("warningscreen.png");
-        blackTexture = new Texture("pause_screen.png"); 
-        stage = new Stage(new FitViewport(1280, 720));
 
-        stage.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-                isExiting = true; 
-            }
-        });
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(1280.0F, 720.0F, camera);
+        viewport.apply();
+        camera.position.set(640.0F, 360.0F, 0.0F);
+
+        Gdx.input.setInputProcessor(null);
     }
 
     @Override
     public void render(float delta) {
-        if (isExiting) {
-            fadeAlpha += delta * fadeSpeed; 
-            if (fadeAlpha >= 1.0f) {
-                game.setScreen(new GameScreen(game)); 
-            }
-        }
-
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(0.0F, 0.0F, 0.0F, 1.0F);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        if (!inputSet) {
-            Gdx.input.setInputProcessor(stage);
-            inputSet = true;
-        }
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
 
-        batch.setProjectionMatrix(stage.getCamera().combined);
         batch.begin();
-        batch.draw(warningTexture, 0, 0, 1280, 720);
-        if (fadeAlpha > 0) {
-            batch.setColor(0, 0, 0, fadeAlpha); 
-            batch.draw(blackTexture, 0, 0, 1280, 720);
-            batch.setColor(1, 1, 1, 1); 
-        }
+        batch.draw(warningTexture, 0.0F, 0.0F, 1280.0F, 720.0F);
         batch.end();
 
-        stage.act(delta);
-        stage.draw();
+        if (Gdx.input.justTouched() || Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
+            Gdx.input.setInputProcessor(null);
+            game.setScreen(new GameScreen(game));
+        }
     }
 
     @Override
     public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
+        viewport.update(width, height, true);
     }
-
-    @Override
-    public void hide() {
-        Gdx.input.setInputProcessor(null);
-        inputSet = false;
-    }
-
-    @Override public void pause() {}
-    @Override public void resume() {}
-
 
     @Override
     public void dispose() {
-        if (batch != null) batch.dispose();
-        if (warningTexture != null) warningTexture.dispose();
-        if (blackTexture != null) blackTexture.dispose();
-        if (stage != null) stage.dispose();
+        batch.dispose();
+        warningTexture.dispose();
     }
+
+    @Override public void hide() {}
+    @Override public void pause() {}
+    @Override public void resume() {}
 }

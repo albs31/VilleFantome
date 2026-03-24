c8d5dae07f636d59d1170435c92f02c899130ec5
Riti <ritikarthikeyan@gmail.com>
2026-03-23 21:56:05 -0700

    Create HouseScreen1.java

---
+++ core/src/main/java/apcs/VilleFantome/HouseScreen1.java
@@ -0,0 +1,58 @@
+package apcs.VilleFantome;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+public class House1Screen implements Screen {
+    private Main game;
+    private SpriteBatch batch;
+    private Stage stage;
+    private Texture background;
+
+    public House1Screen(Main game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        batch = new SpriteBatch();
+        stage = new Stage(new FitViewport(1280, 720));
+        background = new Texture("House1Screen.png");
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(stage.getCamera().combined);
+        batch.begin();
+        batch.draw(background, 0, 0, 1280, 720);
+        batch.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            game.setScreen(new GameScreen(game));
+        }
+    }
+
+    @Override public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void hide() {}
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        stage.dispose();
+        background.dispose();
+    }
+}
